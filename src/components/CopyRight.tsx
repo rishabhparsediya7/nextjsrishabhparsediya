f493544c09f4c24ad13742d06c7216f992f5ceb7
rishabhparsediya7 <parsediyarishabh@gmail.com>
2025-03-02 13:01:58 +0530

    fix: footer theme

--- src/components/CopyRight.tsx
+++ src/components/CopyRight.tsx
@@ -1,11 +1,14 @@
 import { Copyright } from "lucide-react";
 
 export function CopyRight() {
-    return (
-        <div className="w-full h-12 bg-black">
-            <div className="flex h-full items-center">
-                <h1 className="flex m-auto"><Copyright /> &nbsp; All rights reserved @RishabhParsediya </h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full h-12 bg-gray-100 dark:bg-black transition-colors duration-300">
+      <div className="flex h-full items-center">
+        <h1 className="flex m-auto text-black dark:text-white text-sm font-medium">
+          <Copyright className="text-black dark:text-white" />
+          &nbsp; All rights reserved @RishabhParsediya
+        </h1>
+      </div>
+    </div>
+  );
 }
--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
-import { Github, Linkedin, Mail } from 'lucide-react';
-import Link from 'next/link';
+import React from "react";
+import { Github, Linkedin, Mail } from "lucide-react";
+import Link from "next/link";
 
 interface LinkType {
-    link: string,
-    name: JSX.Element
+  link: string;
+  name: JSX.Element;
 }
 
 export function Links() {
+  const iconsArray: LinkType[] = [
+    {
+      link: "https://github.com/rishabhparsediya7",
+      name: <Github className="text-black dark:text-white" />,
+    },
+    {
+      link: "https://www.linkedin.com/in/rishabh-parsediya/",
+      name: <Linkedin className="text-black dark:text-white" />,
+    },
+    {
+      link: "mailto:parsediyarishabh@gmail.com",
+      name: <Mail className="text-black dark:text-white" />,
+    },
+  ];
 
-    const iconsArray: LinkType[] = [
-        { link: "https://github.com/rishabhparsediya7", name: < Github /> },
-        { link: "https://www.linkedin.com/in/rishabh-parsediya/", name: < Linkedin /> },
-        { link: "mailto:parsediyarishabh@gmail.com", name: <Mail /> }
-    ];
-
-    return (
-        <div className="w-full bg-[#161616] py-10">
-            <div className="flex h-full my-auto justify-center space-x-12 items-center">
-                {iconsArray.map((icon: LinkType, index) => (
-                    <div key={index} className="mx-2 bg-black p-3 rounded-full">
-                        <Link href={icon.link} >
-                            {icon.name}
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full bg-gray-100 dark:bg-[#161616] py-10 transition-colors duration-300">
+      <div className="flex h-full my-auto justify-center space-x-12 items-center">
+        {iconsArray.map((icon: LinkType, index) => (
+          <div
+            key={index}
+            className="mx-2 bg-white dark:bg-black p-3 rounded-full 
+                            border border-black/10 dark:border-white/10
+                            hover:bg-gray-100 dark:hover:bg-zinc-900
+                            hover:scale-110 
+                            shadow-lg dark:shadow-white/5
+                            transition-all duration-300"
+          >
+            <Link href={icon.link} target="_blank">
+              {icon.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
