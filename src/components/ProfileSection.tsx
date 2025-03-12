88561c0dccb449d0ec50ef6ccad0d41361a6a384
rishabhparsediya7 <parsediyarishabh@gmail.com>
2025-03-13 00:38:32 +0530

    fix: component height

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -3,7 +3,7 @@ import { SparklesPreview } from "./SparklesPreview";
 
 function ProfileSection() {
   return (
-    <div id="sec" className="flex flex-col h-full w-full max-w-full">
+    <div id="sec" className="flex flex-col h-screen  w-full max-w-full">
       <div className="w-full">
         <SparklesPreview />
       </div>
