7ceb72ccb2e0bd94e032eb127726cd117d8cfc8e
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-08-27 02:11:51 +0530

    sidebar wrapper

---
+++ src/components/SidebarWrapper.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { createPortal } from 'react-dom';
+
+const Sidebar = dynamic(() => import('./sidebar'), { ssr: false });
+
+const SidebarWrapper = () => {
+  const [mounted, setMounted] = useState(false);
+  const [mountedMobile, setMountedMobile] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    setMountedMobile(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <>
+      {mounted &&
+        typeof window !== 'undefined' &&
+        createPortal(
+          <Sidebar />,
+          document.getElementById('sidebar-portal') as HTMLElement
+        )}
+      
+      {mountedMobile &&
+        typeof window !== 'undefined' &&
+        createPortal(
+          <div id="mobile-menu-button-content"></div>,
+          document.getElementById('mobile-menu-button') as HTMLElement
+        )}
+    </>
+  );
+};
+
+export default SidebarWrapper;
