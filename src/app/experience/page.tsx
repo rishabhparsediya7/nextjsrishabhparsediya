a838c83e3b4ff2ce29364c73a3303b37a5b65107
Rishabh Parsediya <parsediyarishabh@gmail.com>
2025-11-01 21:32:17 +0530

    fix: mobile resppnsiveness

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { content } from '@/data/experience';
 import { FiBriefcase, FiCalendar } from 'react-icons/fi';
 
-interface ExperienceItem {
-  title: string;
-  company: string;
-  timeline: string;
-  description: React.ReactNode;
-  content?: React.ReactNode;
-}
-
 const Experience = () => {
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const isMobileMenu = width < 768;
+
+  useEffect(() => {
+    if(typeof window !== 'undefined') {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
   return (
     <div className="min-h-screen py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-4xl mx-auto">
@@ -28,17 +32,17 @@ const Experience = () => {
         </motion.div>
 
         <div className="relative">
-          <div className="absolute left-8 md:left-1/2 w-0.5 h-full bg-gradient-to-b from-blue-400 to-purple-500 transform -translate-x-1/2"></div>
-          
+          {width > 768 && <div className="absolute left-8 md:left-1/2 w-0.5 h-full bg-gradient-to-b from-blue-400 to-purple-500 transform -translate-x-1/2"></div>
+          }
           {content.map((item, index) => (
             <motion.div
               key={index}
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
-              className={`relative mb-8 ${index % 2 === 0 ? 'md:pl-8 md:pr-0 pr-6' : 'md:pr-8 md:pl-0 pl-6'} md:w-[calc(50%-16px)] ${index % 2 === 0 ? 'md:ml-0' : 'md:ml-auto'}`}
+              className={`relative mb-8 ${index % 2 === 0 ? `md:pl-8 md:pr-0 ${isMobileMenu ? '':'pr-6'}` : `md:pr-8 md:pl-0 ${isMobileMenu ? '':'pl-6'}`} md:w-[calc(50%-16px)] ${index % 2 === 0 ? 'md:ml-0' : 'md:ml-auto'}`}
             >
-              <div className={`absolute top-5 ${index % 2 === 0 ? 'md:-left-2 left-2' : 'md:-right-2 right-2'} w-3 h-3 rounded-full bg-gradient-to-r from-blue-500 to-purple-600 shadow-lg`}></div>
+              {isMobileMenu ? null : <div className={`absolute top-5 ${index % 2 === 0 ? 'md:-left-2 left-2' : 'md:-right-2 right-2'} w-3 h-3 rounded-full bg-gradient-to-r from-blue-500 to-purple-600 shadow-lg`}></div>}
               
               <div className="bg-white dark:bg-gray-900/50 rounded-xl shadow-lg p-6 hover:shadow-xl transition-shadow duration-300">
                 <div className="flex items-center mb-2">
